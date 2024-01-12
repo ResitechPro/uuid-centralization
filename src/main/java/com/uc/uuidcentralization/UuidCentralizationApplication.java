e0a64603fd08fe49c06ea0b5bddd9d252d262e67
MohamedJamh <jam7.mohamed@gmail.com>
2024-01-12 23:01:40 +0100

    git commit -m "RESPRO-15 setup"

---
+++ src/main/java/com/uc/uuidcentralization/UuidCentralizationApplication.java
@@ -0,0 +1,13 @@
+package com.uc.uuidcentralization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UuidCentralizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UuidCentralizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uc/uuidcentralization/UuidCentralizationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uc.uuidcentralization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class UuidCentralizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
