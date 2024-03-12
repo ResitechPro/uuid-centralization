82997bac4d56bde645fa28439bfaf734292cc0f8
MohamedJamh <jam7.mohamed@gmail.com>
2024-03-12 14:16:08 +0000

    main - add : adding dependencies for eureka client discovery

--- src/main/java/com/uc/uuidcentralization/UuidCentralizationApplication.java
+++ src/main/java/com/uc/uuidcentralization/UuidCentralizationApplication.java
@@ -2,7 +2,9 @@ package com.uc.uuidcentralization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UuidCentralizationApplication {
 
